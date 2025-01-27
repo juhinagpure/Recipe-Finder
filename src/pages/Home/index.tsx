2844a17423fd0910cebfd59b39e1f6607c3d67eb
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-01-27 22:16:17 +0530

    remove background gradient from main styles and clean up home page paragraph

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,7 +14,7 @@ const Home = () => {
           find inspiration for every occasion.
         </p> */}
 
-        <p className="text-2xl md:text-3xl font-medium mb-10 text-center">
+        <p className="text-2xl md:text-3xl font-medium mb-10">
           Discover delicious recipes tailored to your taste buds! <br />
           From quick snacks to gourmet meals, <br />
           find inspiration for every occasion.
